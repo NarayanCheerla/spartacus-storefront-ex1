e54e51110f40970fcc5bf8e4acd4225558f5c986
Narayana Cheerla <Narayana.Cheerla@jbhunt.com>
2020-01-06 10:41:29 +0000

    angular material integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { StorefrontModule } from '@spartacus/storefront';
 
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { RatingModule } from 'primeng/rating';
 import { SpinnerModule } from 'primeng/spinner';
 import { CarouselModule } from 'primeng/carousel';
-import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app.router.module';
 import { SlateComponent } from './slate/slate.component';
@@ -21,7 +21,7 @@ import { ProductSummaryComponent } from './product-summary/product-summary.compo
     FormsModule,
     SpinnerModule,
     CarouselModule,
-    ButtonModule,
+    MatButtonModule,
     StorefrontModule.withConfig({
       server: {
         baseUrl: 'https://storefront.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com'
