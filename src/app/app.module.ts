f96323438fd55d77e1b3721506de5af21252322d
Narayana Cheerla <Narayana.Cheerla@jbhunt.com>
2020-01-06 09:44:39 +0000

    added custom routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,23 @@ import { StorefrontModule } from '@spartacus/storefront';
 
 import { AppComponent } from './app.component';
 import { RatingModule } from 'primeng/rating';
-import { SpinnerModule } from 'primeng/spinner';;
+import { SpinnerModule } from 'primeng/spinner';
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app.router.module';
 import { SlateComponent } from './slate/slate.component';
+import { ProductSummaryComponent } from './product-summary/product-summary.component';
 @NgModule({
-  declarations: [AppComponent, SlateComponent],
+  declarations: [AppComponent, SlateComponent, ProductSummaryComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
     RatingModule,
     FormsModule,
     SpinnerModule,
+    CarouselModule,
+    ButtonModule,
     StorefrontModule.withConfig({
       server: {
         baseUrl: 'https://storefront.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com'
